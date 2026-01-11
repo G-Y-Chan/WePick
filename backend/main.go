e7d37449e2869b804b8ac2f97d87aefeb67041ae
G-Y-Chan <changuanyu2904@gmail.com>
2026-01-11 18:05:56 +0800

    Refactor backend by separating handlers from core application logic

--- backend/main.go
+++ backend/main.go
@@ -1,52 +1,8 @@
 package main
 
 import (
-    "fmt"
     "net/http"
-	"encoding/json"
-	"main/util"
 )
-
-func test(w http.ResponseWriter, req *http.Request) {
-	// Inform client that the response type is JSON
-	w.Header().Set("Content-Type", "application/json")
-    // Set the HTTP status code (optional, http.StatusOK is 200).
-	w.WriteHeader(http.StatusOK)
-	m := util.Message{"testing", "This is a placeholder body."}
-	if err := json.NewEncoder(w).Encode(m); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
-}
-
-func postEmail(w http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-
-	var email string
-
-	// Use json.NewDecoder to decode the stream directly into the struct
-	err := json.NewDecoder(req.Body).Decode(&email)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	} else {
-		fmt.Print(email)
-	}
-
-	// Inform client that the response type is JSON
-	w.Header().Set("Content-Type", "application/json")
-    // Set the HTTP status code (optional, http.StatusOK is 200).
-	w.WriteHeader(http.StatusOK)
-}
 
 func main() {
 	http.HandleFunc("/test", test)
--- backend/room_handlers.go
+++ backend/room_handlers.go
@@ -3,15 +3,9 @@ package main
 import (
 	"encoding/json"
 	"main/util"
-	"math/rand"
 	"net/http"
-	"slices"
-	"strconv"
 	"fmt"
 )
-
-var intervals []util.Interval
-var max = 1000000
 
 func getRoomCode(w http.ResponseWriter, req *http.Request) {
 	// Inform client that the response type is JSON
@@ -23,76 +17,6 @@ func getRoomCode(w http.ResponseWriter, req *http.Request) {
 	if err := json.NewEncoder(w).Encode(m); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	}
-}
-
-func generateWrapper() string {
-	paddedLength := 6
-	var code int
-	code = generateRoomCode()
-	insertRoomCode(code)
-	if code >= 100000 {
-		return strconv.Itoa(code)
-	} else {
-		var temp = strconv.Itoa(code)
-		for len(temp) < paddedLength {
-			temp = "0" + temp
-		}
-		return temp
-	}
-}
-
-func generateRoomCode() int {
-	var code int
-	code = rand.Intn(max)
-	if len(intervals) == 0 {
-		return code
-	}
-	for i := 0; i < len(intervals); i++ {
-		s := intervals[i].Start
-		e := intervals[i].End
-		if code >= e {
-			code += (e-s+1)
-		} else if code >= s {
-			code := e + (code-s+1)
-			return code
-		} else {
-			return code
-		}
-	}
-	max := max - 1
-	_ = max
-	return code
-}
-
-// Logic behind generating new room code guarantees validity of code.
-// Thus, no checks required before inserting new code into intervals.
-func insertRoomCode(code int) {
-	var newIntervalIndex = -1
-	for i := 0; i < len(intervals); i++ {
-		s := intervals[i].Start
-		e := intervals[i].End
-		if code >= e {
-			if code == e+1 {
-				intervals[i].End = code
-				return
-			}
-		} else {
-			if code+1 == s {
-				intervals[i].Start = code
-				return
-			} else {
-				newIntervalIndex = i
-				break
-			}
-		}
-	}
-	// New code larger than all previous ones
-	if newIntervalIndex == -1 {
-		intervals = append(intervals, util.Interval{code, code})
-	} else {
-		newInterval := util.Interval{code, code}
-		intervals = slices.Insert(intervals, newIntervalIndex, newInterval)
 	}
 }
 
@@ -121,19 +45,6 @@ func verifyRoomCode(w http.ResponseWriter, req *http.Request) {
 
     // Set the HTTP status code (optional, http.StatusOK is 200).
 	w.WriteHeader(http.StatusOK)
-}
-
-func verifyWrapper(roomCode string) string {
-	var intCode, err = strconv.Atoi(roomCode)
-	if err != nil {
-		return "false"
-	}
-	for i := 0; i < len(intervals); i++ {
-		if intCode >= intervals[i].Start && intCode <= intervals[i].End {
-			return "true"
-		}
-	}
-	return "false"
 }
 
 func verifyStart(w http.ResponseWriter, req *http.Request) {
